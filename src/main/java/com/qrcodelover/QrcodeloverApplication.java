302bce2e6c64dc4e4d999334bcc58657d2e0c770
Larissa Faria <lari.f4ria@gmail.com>
2025-07-28 10:02:47 -0300

    create project

---
+++ src/main/java/com/qrcodelover/QrcodeloverApplication.java
@@ -0,0 +1,13 @@
+package com.qrcodelover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeloverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodeloverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qrcodelover/QrcodeloverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qrcodelover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeloverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
